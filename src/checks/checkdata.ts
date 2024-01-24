4670ba383b57774ba1fbb8969f1a9b71d2429944
wechuli <wechulipaul@yahoo.com>
2024-01-24 17:33:53 +0300

    add

---
+++ src/checks/checkdata.ts
@@ -0,0 +1,11 @@
+let data = {
+    "name": "test",
+    "head_sha": "1234",
+    "status": "completed",
+    "conclusion": "success",
+    "output": {
+        "title": "test",
+        "summary": "test",
+        "text": "test"
+    }
+}
