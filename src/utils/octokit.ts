a032578fe8153b2d883dea7c0a6dfd7f7020e339
wechuli <wechulipaul@yahoo.com>
2024-01-29 18:40:01 +0300

    reduce oktokit logs

--- src/utils/octokit.ts
+++ src/utils/octokit.ts
@@ -2,15 +2,18 @@ import * as octokit from "@octokit/rest";
 import * as core from "@actions/core";
 
 export const restClient = new octokit.Octokit({
-  auth: core.getInput("token"),
-  userAgent: "allcheckspassed-action",
-  baseUrl: process.env.GITHUB_API_URL || "https://api.github.com",
-  log: {
-    debug: console.debug,
-    info: console.info,
-    warn: console.warn,
-    error: console.error,
-  },
+    auth: core.getInput("token"),
+    userAgent: "allcheckspassed-action",
+    baseUrl: process.env.GITHUB_API_URL || "https://api.github.com",
+    log: {
+        // don't log info and debug messages
+        info: () => {
+        },
+        debug: () => {
+        },
+        warn: console.warn,
+        error: console.error,
+    },
 });
 
 
